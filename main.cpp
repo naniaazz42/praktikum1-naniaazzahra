2f1f03caff7c7ae2926aa3760c077b34715f73c8
nania <azzahranania04@gmail.com>
2023-10-14 23:47:44 -0700

    menambahkankan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "------------------------------------------------------------------------------" << endl << endl;
+    cout << "Nama : Nania Azzahra" << endl << endl;
+    cout << "NPM : 2310631170037" << endl << endl;
+    cout << "Kelas : A - Informatika" << endl << endl;
+    cout << "Cita - cita : Guru" << endl << endl;
+    cout << "Motivasi Masuk Informatika : Karena ingin memperdalam tentang program komputer" << endl << endl;
+    cout << "------------------------------------------------------------------------------" << endl << endl;
+    return 0;
+}
